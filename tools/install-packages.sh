8e3b55e679b40b74c692bf57532f7b1d82a9dd4e
gobbyo <gobbyo@microsoft.com>
2021-01-20 20:16:57 +0000

    removed old conversion tool

--- tools/install-packages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-apt-get install -y git cmake make g++ lcov clang-format-9 ccache
--- tools/src/vspec-to-dtdl/run-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./vspec-to-dtdl ../../third-party/vehicle_signal_specification/src/vehicle_signal_specification/spec/VehicleSignalSpecification.vspec
